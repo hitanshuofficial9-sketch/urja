28007cb8ef1c9773bef3ce2ffb88893d725fa893
Urja <urja@gmail.com>
2026-01-08 12:18:26 +0530

    packing unpacking  and loops

---
+++ tuples .py
@@ -0,0 +1,23 @@
+a=(10,20,30)
+print(a[2])
+
+b=(10,20,30)
+c=list(b)
+c[1]=30
+print(c)
+
+a=(1,5,7)
+b=(4,9)
+print(a+b) 
+print(a*3)
+
+fruits=("apple","mango","cherry")
+(red,yellow,purple)=fruits
+print(red)
+print(purple)
+
+fruits=("apple","mango","cherry")
+for i in fruits:
+    print(i)
+
+    
