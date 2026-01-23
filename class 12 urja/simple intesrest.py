e804b67da68a334777b82c8d22e8293e61940730
Urja <urja@gmail.com>
2026-01-23 12:13:08 +0530

    simple interest

---
+++ class 12 urja/simple intesrest.py
@@ -0,0 +1,7 @@
+def simpleinterest():
+    p=int(input("enter yout principle amount"))
+    r=int(input("enter the rate of interest"))
+    t=int(input("enter time in month"))
+    print((p*r*t)/(100*12))
+
+simpleinterest()    
