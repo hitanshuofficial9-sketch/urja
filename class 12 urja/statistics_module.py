4e7ea5148572d1f5dec3a918e805ee4758fbb316
Urja <urja@gmail.com>
2026-04-23 11:00:32 +0530

    statistics module

---
+++ class 12 urja/statistics_module.py
@@ -0,0 +1,12 @@
+import statistics
+data=[10,20,30,40,50]
+print(statistics.mean(data))
+print(statistics.fmean(data))
+print(statistics.geometric_mean(data))
+print(statistics.harmonic_mean(data))
+print(statistics.median(data))
+print(statistics.median_high(data))
+print(statistics.median_low(data))
+print(statistics.mode(data))
+print(statistics.variance(data))
+print(statistics.stdev(data))
